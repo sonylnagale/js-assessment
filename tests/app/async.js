689c95022eba264840577beca67d47f762deae86
sonyl <sonyl@192.168.0.5>
2016-02-04 11:11:56 -0500

    Passes retrieving from server

--- tests/app/async.js
+++ tests/app/async.js
@@ -36,7 +36,22 @@ describe('async behavior', function() {
   it('you should be able to retrieve data from the server and return a sorted array of names', function(done) {
     var url = '/data/testdata.json';
     
+    asyncAnswers.manipulateRemoteData = function(url) {    	
+    	return $.getJSON(url).then(function(data) {
+    		var people = new Array();
+    		
+    		for (var i = 0; i < data.people.length; i++) {
+    			people.push(data.people[i].name);
+    		}
+	
+    		return people.sort();
+	    });
+    	
+
+    };
+    
     asyncAnswers.manipulateRemoteData(url).then(function(result) {
+    	console.log(result)
       expect(result).to.have.length(5);
       expect(result.join(' ')).to.eql('Adam Alex Matt Paul Rebecca');
       done();
