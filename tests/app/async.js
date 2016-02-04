2c75dc505585c7f94dfcc3aeaed2609d3318f6db
sonyl <sonyl@192.168.0.5>
2016-02-04 10:33:03 -0500

    Passes promises

--- tests/app/async.js
+++ tests/app/async.js
@@ -11,8 +11,13 @@ describe('async behavior', function() {
 
     function finish(done) {
       if (++finished === total) { done(); }
-    }
-
+    }	
+    
+    asyncAnswers.async = function(value) {
+    	var flag = Promise.resolve(value);
+    	return flag;
+    };
+    
     asyncAnswers.async(true).then(function(result) {
       flag = result;
       expect(flag).to.eql(true);
@@ -30,7 +35,7 @@ describe('async behavior', function() {
 
   it('you should be able to retrieve data from the server and return a sorted array of names', function(done) {
     var url = '/data/testdata.json';
-
+    
     asyncAnswers.manipulateRemoteData(url).then(function(result) {
       expect(result).to.have.length(5);
       expect(result.join(' ')).to.eql('Adam Alex Matt Paul Rebecca');
